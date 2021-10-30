69537bb62b6fe37d7df5558b7148562de6bc1fa4
nathomp3 <nathomp3@ncsu.edu>
2021-10-29 23:19:34 -0400

    Initial commit

---
+++ Items/StoneStaff.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DubNation.Items
+{
+	class StoneStaff : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Stone Staff");
+			Tooltip.SetDefault("Summons a boulder minion to fight for you. Breaks after 10 attacks.");
+			ItemID.Sets.GamepadWholeScreenUseRange[item.type] = true;
+			ItemID.Sets.LockOnIgnoresCollision[item.type] = true;
+		}
+
+		public override void SetDefaults()
+		{
+			item.damage = 21;
+			item.width = 40;
+			item.height = 40;
+			item.useTime = 60;
+			item.useAnimation = 60;
+			item.useStyle = ItemUseStyleID.SwingThrow;
+			item.value = 10;
+			item.rare = ItemRarityID.White;
+			item.UseSound = SoundID.Item1;
+			item.mana = 10;
+			item.noMelee = true;
+			item.summon = true;
+			item.buffType = ModContent.BuffType<Buffs.StoneBuff>();
+			item.shoot = ModContent.ProjectileType<Projectiles.StoneMinion>();
+		}
+
+		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+		{
+			player.AddBuff(item.buffType, 2);
+			position = Main.MouseWorld;
+			return true;
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.StoneBlock, 36);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
---
+++ Items/WoodStaff.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+
+namespace DubNation.Items
+{
+	public class WoodStaff : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Wood Staff");
+			Tooltip.SetDefault("Summons a coat rack minion to fight for you");
+			ItemID.Sets.GamepadWholeScreenUseRange[item.type] = true;
+			ItemID.Sets.LockOnIgnoresCollision[item.type] = true;
+		}
+
+		public override void SetDefaults()
+		{
+			item.damage = 6;
+			item.width = 40;
+			item.height = 40;
+			item.useTime = 36;
+			item.useAnimation = 36;
+			item.useStyle = ItemUseStyleID.SwingThrow;
+			item.value = 10;
+			item.rare = ItemRarityID.White;
+			item.UseSound = SoundID.Item1; 
+			item.mana = 1;
+			item.noMelee = true;
+			item.summon = true;
+			item.buffType = ModContent.BuffType<Buffs.WoodBuff>();
+			item.shoot = ModContent.ProjectileType<Projectiles.WoodMinion>();
+		}
+
+		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+		{
+			player.AddBuff(item.buffType, 2);
+			position = Main.MouseWorld;
+			return true;
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.Wood, 12);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
