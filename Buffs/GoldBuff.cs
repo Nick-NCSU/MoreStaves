160101665342811accef08f1d7190a12236d5bd7
nathomp3 <nathomp3@ncsu.edu>
2021-10-30 14:24:22 -0400

    Added Gold/Platinum Minions

---
+++ Buffs/GoldBuff.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DubNation.Buffs
+{
+	public class GoldBuff : ModBuff
+	{
+		public override void SetDefaults()
+		{
+			DisplayName.SetDefault("Gold Minion");
+			Description.SetDefault("The gold minion will fight for you");
+			Main.buffNoSave[Type] = true;
+			Main.buffNoTimeDisplay[Type] = true;
+		}
+
+		public override void Update(Player player, ref int buffIndex)
+		{
+			if (player.ownedProjectileCounts[ModContent.ProjectileType<Projectiles.GoldMinion>()] > 0)
+			{
+				player.buffTime[buffIndex] = 18000;
+			}
+			else
+			{
+				player.DelBuff(buffIndex);
+				buffIndex--;
+			}
+		}
+	}
+}
---
+++ Projectiles/GoldProjectile.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DubNation.Projectiles
+{
+    class GoldProjectile : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.projPet[projectile.type] = true;
+            ProjectileID.Sets.Homing[projectile.type] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.width = 6;
+            projectile.height = 16;
+            projectile.friendly = true;
+            projectile.minion = true;
+            projectile.penetrate = 1;
+            projectile.timeLeft = 600;
+            projectile.ignoreWater = true;
+            projectile.tileCollide = true;
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            projectile.Kill();
+            return true;
+        }
+
+        public override bool MinionContactDamage()
+        {
+            return true;
+        }
+
+		public override void AI()
+		{
+            #region Movement
+            float inertia = 20f;
+            Vector2 direction = new Vector2(0, 0.5f);
+            projectile.velocity = (projectile.velocity * (inertia - 1) + direction) / inertia;
+            #endregion
+
+            #region Animation and visuals
+            // So it will lean slightly towards the direction it's moving
+            projectile.rotation = projectile.velocity.X * 0.05f;
+
+			// This is a simple "loop through all frames from top to bottom" animation
+			int frameSpeed = 5;
+			projectile.frameCounter++;
+			if (projectile.frameCounter >= frameSpeed)
+			{
+				projectile.frameCounter = 0;
+				projectile.frame++;
+				if (projectile.frame >= Main.projFrames[projectile.type])
+				{
+					projectile.frame = 0;
+				}
+			}
+
+			// Some visuals here
+			Lighting.AddLight(projectile.Center, Color.White.ToVector3() * 0.78f);
+			#endregion
+
+		}
+	}
+}
