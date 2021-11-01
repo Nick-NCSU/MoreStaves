0f1f811ea07bc894a38bb76ef45a4a7300936eb8
nathomp3 <nathomp3@ncsu.edu>
2021-11-01 17:09:42 -0400

    Added spectre minion

---
+++ Projectiles/SpectreProjectile.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DubNation.Projectiles
+{
+    class SpectreProjectile : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.projPet[projectile.type] = true;
+            ProjectileID.Sets.Homing[projectile.type] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.width = 24;
+            projectile.height = 32;
+            projectile.friendly = true;
+            projectile.minion = true;
+            projectile.penetrate = 3;
+            projectile.timeLeft = 300;
+            projectile.ignoreWater = true;
+            projectile.tileCollide = false;
+        }
+
+        public override bool MinionContactDamage()
+        {
+            return true;
+        }
+
+		public override void AI()
+		{
+
+            #region Animation and visuals
+            if (projectile.ai[0]++ >= 360)
+            {
+                projectile.ai[0] = 0;
+            }
+            projectile.rotation = projectile.ai[0]/5;
+            // Some visuals here
+            Lighting.AddLight(projectile.Center, Color.White.ToVector3() * 0.78f);
+			#endregion
+
+		}
+	}
+}
