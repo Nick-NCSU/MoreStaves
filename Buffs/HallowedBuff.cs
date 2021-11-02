ec8d5c76ac8ca7ebd8e3d72a41bcc70bd894d986
nathomp3 <nathomp3@ncsu.edu>
2021-11-01 22:09:06 -0400

    Added hallowed minion

---
+++ Buffs/HallowedBuff.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DubNation.Buffs
+{
+	public class HallowedBuff : ModBuff
+	{
+		public override void SetDefaults()
+		{
+			DisplayName.SetDefault("Hallowed Minion");
+			Description.SetDefault("The hallowed minion will fight for you");
+			Main.buffNoSave[Type] = true;
+			Main.buffNoTimeDisplay[Type] = true;
+		}
+
+		public override void Update(Player player, ref int buffIndex)
+		{
+			if (player.ownedProjectileCounts[ModContent.ProjectileType<Projectiles.HallowedMinion>()] > 0)
+			{
+				player.buffTime[buffIndex] = 18000;
+			}
+			else
+			{
+				player.DelBuff(buffIndex);
+				buffIndex--;
+			}
+		}
+	}
+}
